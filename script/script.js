357657e954c4cf7eb47fe1b4a07684c4c16857c7
9JuanDiaz <139511784+9JuanDiaz@users.noreply.github.com>
2024-07-28 23:12:54 -0400

    Cambio en error de scrip

---
+++ script/script.js
@@ -0,0 +1,11 @@
+console.log('Hello, World este es un cambio!');
+
+function crearNuevoElemento(tagName, contenido) {
+    const nuevoElemento = document.createElement(tagName);
+    nuevoElemento.textContent = contenido;
+    document.body.appendChild(nuevoElemento);
+  }
+  
+  // Ejemplo de uso:
+  crearNuevoElemento('p', '¡Hola desde una nueva función!');
+  
