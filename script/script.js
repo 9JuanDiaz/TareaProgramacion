0674eac8f120e6ff5fc57e206852655b25eb5f71
9JuanDiaz <139511784+9JuanDiaz@users.noreply.github.com>
2024-07-28 22:50:16 -0400

    Actualizar script.js con nuevos cambios

--- script/script.js
+++ script/script.js
@@ -1,1 +1,11 @@
 console.log('Hello, World!');
+
+function crearNuevoElemento(tagName, contenido) {
+    const nuevoElemento = document.createElement(tagName);
+    nuevoElemento.textContent = contenido;
+    document.body.appendChild(nuevoElemento);
+  }
+  
+  // Ejemplo de uso:
+  crearNuevoElemento('p', '¡Hola desde una nueva función!');
+  
